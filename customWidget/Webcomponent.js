6f00216f05651ae6f16da3a1c2aa0cefc4ba9d01
mridul0007 <mridul0007@gmail.com>
2023-07-20 15:45:58 +0200

    adding new buttons

--- customWidget/Webcomponent.js
+++ customWidget/Webcomponent.js
@@ -1,40 +1,72 @@
-(function () {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = 
-    `<button type="button" id="button_modify">MODIFY</button>` 
-    `<button type="button" id="button_delete">DELETE</button>` 
-    `<button type="button" id="button_create">CREATE</button>` ;
+// (function () {
+//     let tmpl = document.createElement('template');
+//     tmpl.innerHTML = 
+//     `<button type="button" id="button_modify">MODIFY</button>` 
+//     `<button type="button" id="button_delete">DELETE</button>` 
+//     `<button type="button" id="button_create">CREATE</button>` ;
     
 
    
-    class PerformanceHelp extends HTMLElement {
-        constructor(height) {
-            console.log(height);
-            super();
-            this.init();           
-        }
+//     class PerformanceHelp extends HTMLElement {
+//         constructor(height) {
+//             console.log(height);
+//             super();
+//             this.init();           
+//         }
 
-        init() {            
+//         init() {            
               
-            let shadowRoot = this.attachShadow({mode: "open"});
-            shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this.addEventListener("click", event => {
-            var event = new Event("onClick");
-            this.fireChanged();           
-            this.dispatchEvent(event);
-            });           
-        }
+//             let shadowRoot = this.attachShadow({mode: "open"});
+//             shadowRoot.appendChild(tmpl.content.cloneNode(true));
+//             this.addEventListener("click", event => {
+//             var event = new Event("onClick");
+//             this.fireChanged();           
+//             this.dispatchEvent(event);
+//             });           
+//         }
 
-        fireChanged() {
-            console.log(this.dept);
-            console.log("OnClick Triggered");     
+//         fireChanged() {
+//             console.log(this.dept);
+//             console.log("OnClick Triggered");     
             
-        }    
-        test(){
-            console.log(this.dept);
-        }
+//         }    
+//         test(){
+//             console.log(this.dept);
+//         }
         
-    }
+//     }
 
+//     customElements.define('custom-button', PerformanceHelp);
+// })();
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+      <button type="button" id="button_modify">MODIFY</button>
+      <button type="button" id="button_delete">DELETE</button>
+      <button type="button" id="button_create">CREATE</button>
+    `;
+  
+    class PerformanceHelp extends HTMLElement {
+      constructor() {
+        super();
+        this.init();
+      }
+  
+      init() {
+        let shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(tmpl.content.cloneNode(true));
+        this.addEventListener('click', (event) => {
+          var event = new Event('onClick');
+          this.fireChanged();
+          this.dispatchEvent(event);
+        });
+      }
+  
+      fireChanged() {
+        console.log('OnClick Triggered');
+      }
+    }
+  
     customElements.define('custom-button', PerformanceHelp);
-})();
+  })();
+  

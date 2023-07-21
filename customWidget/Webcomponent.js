a6bf8e51c67580c71a033ff63109d16688788b3d
mridul0007 <mridul0007@gmail.com>
2023-07-21 14:37:04 +0200

    revert

--- customWidget/Webcomponent.js
+++ customWidget/Webcomponent.js
@@ -241,11 +241,11 @@
 
         fireChanged() {
             console.log('OnClick Triggered');
-            this._props= {...this._props,...changedProperties}
-            const inputBox = shadowRoot.getElementById('input_box');
+            // this._props= {...this._props,...changedProperties}
+            // const inputBox = shadowRoot.getElementById('input_box');
 
-            // Set the value of the input field
-            inputBox.value = this.dept;
+            // // Set the value of the input field
+            // inputBox.value = this.dept;
         
         }
     }

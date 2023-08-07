085cd57d865aa3b2e59d1c47bf88006336d740de
mridul0007 <mridul0007@gmail.com>
2023-08-07 14:26:21 +0200

    new loading screen

--- customWidget/Webcomponent.js
+++ customWidget/Webcomponent.js
@@ -27,6 +27,18 @@
           gap: 10px;
           margin-top: 2cm;
         }
+        /* Add your custom styles for the loading screen here */
+        .loading-screen {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background-color: rgba(0, 0, 0, 0.5);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          z-index: 9999;
       </style>
       <div class="root">
         <label for="input_box">SELECTED INVESTMENT:</label>

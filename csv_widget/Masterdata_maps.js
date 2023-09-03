8c746f913bcad46ee50b71c1b41e65050c63993c
mridul0007 <mridul0007@gmail.com>
2023-09-03 14:35:18 +0200

    new change for csv

--- csv_widget/Masterdata_maps.js
+++ csv_widget/Masterdata_maps.js
@@ -1,56 +1,59 @@
 (function () {
-  // Define your Google Maps API key, latitude, longitude, and zoom level
-  const API_KEY = 'YOUR_API_KEY';
-  const LATITUDE = YOUR_LATITUDE;
-  const LONGITUDE = YOUR_LONGITUDE;
-  const ZOOM_LEVEL = YOUR_ZOOM_LEVEL;
+  
+
 
   let tmpl = document.createElement('template');
   tmpl.innerHTML = `
-    <style>
-      /* Add your custom styles for the map container */
-      #map-widget {
-        width: 100%;
-        height: 400px;
-      }
-    </style>
+  <div class="root">
+    <label for="input_box">Upload file:</label>
+    <input type="file" id="input_box" accept=".csv" style="display: none;">
+    <input type="text" id="file_name" placeholder="Select a CSV file..." readonly>
+    <button id="select_file_button">Select File</button>
+    <button id="upload_button">Upload</button>
+  </div>
   `;
 
-  class MasterData_Maps extends HTMLElement {
+  class MasterData_CSV extends HTMLElement {
     constructor() {
       super();
       this.init();
-      loadGoogleMaps();
+    
     }
 
     init() {
       let shadowRoot = this.attachShadow({ mode: 'open' });
       shadowRoot.appendChild(tmpl.content.cloneNode(true));
-    }
+
+      // Get references to the elements
+      const fileInput = shadowRoot.getElementById('input_box');
+      const fileNameInput = shadowRoot.getElementById('file_name');
+      const selectFileButton = shadowRoot.getElementById('select_file_button');
+      const uploadButton = shadowRoot.getElementById('upload_button');
+
+      // Add a click event listener to the "Select File" button
+      selectFileButton.addEventListener('click', () => {
+        fileInput.click(); // Trigger a click event on the hidden file input
+      });
+
+      // Add a change event listener to the file input
+      fileInput.addEventListener('change', () => {
+        // Display the selected file name in the text input
+        fileNameInput.value = fileInput.files[0].name;
+      });
+
+      // Add a click event listener to the "Upload" button
+      uploadButton.addEventListener('click', () => {
+        // Implement your file upload logic here
+    });
+  }
 
     onCustomWidgetAfterUpdate(ochangedProperties) {}
 
     fireChanged() {}
   }
 
-  customElements.define('custom-widget', MasterData_Maps);
+  customElements.define('custom-widget', MasterData_CSV);
 
-  function loadGoogleMaps() {
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`;
-    script.defer = true;
-    document.head.appendChild(script);
-  }
-
-  function initMap() {
-    const shadowRoot = document.querySelector('custom-widget').shadowRoot;
-    const mapElement = document.createElement('div');
-    mapElement.id = 'map-widget';
-    shadowRoot.appendChild(mapElement);
-
-    const map = new google.maps.Map(mapElement, {
-      center: { lat: LATITUDE, lng: LONGITUDE },
-      zoom: ZOOM_LEVEL,
-    });
-  }
+ 
+  
 })();

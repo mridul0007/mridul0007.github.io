21002fdf74315d9aa57eb651b15592f694b769f0
mridul0007 <mridul0007@gmail.com>
2023-09-04 21:15:48 +0200

    table push

--- csv_widget/h.js
+++ csv_widget/h.js
@@ -1,94 +1,94 @@
-(function () {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-    <div class="root">
-    <label for="input_box">Upload file:</label>
-    <input type="file" id="input_box" accept=".csv" style="display: none;">
-    <input type="text" id="file_name" placeholder="Select a CSV file..." readonly>
-    <button id="select_file_button">Select File</button>
-    <button id="upload_button">Upload</button>
-  </div>
-    `;
+// (function () {
+//     let tmpl = document.createElement('template');
+//     tmpl.innerHTML = `
+//     <div class="root">
+//     <label for="input_box">Upload file:</label>
+//     <input type="file" id="input_box" accept=".csv" style="display: none;">
+//     <input type="text" id="file_name" placeholder="Select a CSV file..." readonly>
+//     <button id="select_file_button">Select File</button>
+//     <button id="upload_button">Upload</button>
+//   </div>
+//     `;
 
-    class MasterData_Maintain extends HTMLElement {
-        constructor() {
-            super();
-            this.init();
-        }
+//     class MasterData_Maintain extends HTMLElement {
+//         constructor() {
+//             super();
+//             this.init();
+//         }
 
-        init() {
-            let shadowRoot = this.attachShadow({ mode: 'open' });
-            shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._export_settings = {};
+//         init() {
+//             let shadowRoot = this.attachShadow({ mode: 'open' });
+//             shadowRoot.appendChild(tmpl.content.cloneNode(true));
+//             this._export_settings = {};
             
             
-            const fileInput = shadowRoot.getElementById('input_box');
-            const fileNameInput = shadowRoot.getElementById('file_name');
-            const selectFileButton = shadowRoot.getElementById('select_file_button');
-            const uploadButton = shadowRoot.getElementById('upload_button');
+//             const fileInput = shadowRoot.getElementById('input_box');
+//             const fileNameInput = shadowRoot.getElementById('file_name');
+//             const selectFileButton = shadowRoot.getElementById('select_file_button');
+//             const uploadButton = shadowRoot.getElementById('upload_button');
       
-            // Add a click event listener to the "Select File" button
-            selectFileButton.addEventListener('click', () => {
-              fileInput.click(); // Trigger a click event on the hidden file input
-            });
+//             // Add a click event listener to the "Select File" button
+//             selectFileButton.addEventListener('click', () => {
+//               fileInput.click(); // Trigger a click event on the hidden file input
+//             });
       
-            // Add a change event listener to the file input
-            fileInput.addEventListener('change', () => {
-              // Display the selected file name in the text input
-              fileNameInput.value = fileInput.files[0].name;
-            });
+//             // Add a change event listener to the file input
+//             fileInput.addEventListener('change', () => {
+//               // Display the selected file name in the text input
+//               fileNameInput.value = fileInput.files[0].name;
+//             });
       
-           // ...
+//            // ...
       
-          // Add a click event listener to the "Upload" button
-          uploadButton.addEventListener('click', () => {
-            // Get the selected file
-            const selectedFile = fileInput.files[0];
-            this.fillData();
-            if (selectedFile) {
-              // Create a FileReader object
-              const reader = new FileReader();
+//           // Add a click event listener to the "Upload" button
+//           uploadButton.addEventListener('click', () => {
+//             // Get the selected file
+//             const selectedFile = fileInput.files[0];
+//             this.fillData();
+//             if (selectedFile) {
+//               // Create a FileReader object
+//               const reader = new FileReader();
       
-              // Define an event handler for when the file is loaded
-              reader.onload = (event) => {
-                const fileContents = event.target.result; // This will contain the file contents
+//               // Define an event handler for when the file is loaded
+//               reader.onload = (event) => {
+//                 const fileContents = event.target.result; // This will contain the file contents
                 
-                // Detect the separator (either comma or semicolon)
-                let separator = ',';
-                if (fileContents.includes(';')) {
-                  separator = ';';
-                }
+//                 // Detect the separator (either comma or semicolon)
+//                 let separator = ',';
+//                 if (fileContents.includes(';')) {
+//                   separator = ';';
+//                 }
       
-                // Split the file contents by the detected separator
-                const lines = fileContents.split('\n');
-                lines.forEach((line) => {
-                  const values = line.split(separator);
-                  console.log('CSV Line:', values);
-                  // You can process each line's values here
-                });
-              };
+//                 // Split the file contents by the detected separator
+//                 const lines = fileContents.split('\n');
+//                 lines.forEach((line) => {
+//                   const values = line.split(separator);
+//                   console.log('CSV Line:', values);
+//                   // You can process each line's values here
+//                 });
+//               };
               
               
-              // Read the file as text
-              reader.readAsText(selectedFile);
-            }
-          });
+//               // Read the file as text
+//               reader.readAsText(selectedFile);
+//             }
+//           });
 
 
-        }
-        async fillData() {
+//         }
+//         async fillData() {
             
  
-            let datasource = this.exportDataSource;
-            const dataBinding = this.dataBindings.getDataBinding('exportDataSource')
-            // this.dataBindings.getDataBinding().addDimensionToFeed("dimensions", 'MK_REGION');
-            let x = dataBinding.getDimensions("dimensions");
-            let y = await dataBinding.getDataSource().getMembers(x,{offset: 0,limit:1});
-            console.log(y);
+//             // let datasource = this.exportDataSource;
+//             // const dataBinding = this.dataBindings.getDataBinding('exportDataSource')
+//             // // this.dataBindings.getDataBinding().addDimensionToFeed("dimensions", 'MK_REGION');
+//             // let x = dataBinding.getDimensions("dimensions");
+//             // let y = await dataBinding.getDataSource().getMembers(x,{offset: 0,limit:1});
+//             // console.log(y);
 
-            datasource.data.forEach(row => {
-                console.log(row);
-            })
+//             // datasource.data.forEach(row => {
+//             //     console.log(row);
+//             // })
 
            
 
@@ -97,19 +97,126 @@
 
 
 
-        }
+//         }
 
 
-        fireChanged() {
-            console.log('OnClick Triggered');
-            // this._props= {...this._props,...changedProperties}
-            // const inputBox = shadowRoot.getElementById('input_box');
+//         fireChanged() {
+//             console.log('OnClick Triggered');
+//             // this._props= {...this._props,...changedProperties}
+//             // const inputBox = shadowRoot.getElementById('input_box');
 
-            // // Set the value of the input field
-            // inputBox.value = this.dept;
+//             // // Set the value of the input field
+//             // inputBox.value = this.dept;
 
-        }
+//         }
+//     }
+
+//     customElements.define('custom-button', MasterData_Maintain);
+// })();
+
+
+(function () {
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+      <div class="root">
+        <label for="input_box">Upload file:</label>
+        <input type="file" id="input_box" accept=".csv" style="display: none;">
+        <input type="text" id="file_name" placeholder="Select a CSV file..." readonly>
+        <button id="select_file_button">Select File</button>
+        <button id="upload_button">Upload</button>
+        <div id="csv_table"></div>
+      </div>
+    `;
+  
+    class MasterData_Maintain extends HTMLElement {
+      constructor() {
+        super();
+        this.init();
+      }
+  
+      init() {
+        let shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(tmpl.content.cloneNode(true));
+  
+        const fileInput = shadowRoot.getElementById('input_box');
+        const fileNameInput = shadowRoot.getElementById('file_name');
+        const selectFileButton = shadowRoot.getElementById('select_file_button');
+        const uploadButton = shadowRoot.getElementById('upload_button');
+        const csvTable = shadowRoot.getElementById('csv_table');
+  
+        let csvData = []; // Array to store CSV data
+  
+        // Add a click event listener to the "Select File" button
+        selectFileButton.addEventListener('click', () => {
+          fileInput.click(); // Trigger a click event on the hidden file input
+        });
+  
+        // Add a change event listener to the file input
+        fileInput.addEventListener('change', () => {
+          // Display the selected file name in the text input
+          fileNameInput.value = fileInput.files[0].name;
+        });
+  
+        // Add a click event listener to the "Upload" button
+        uploadButton.addEventListener('click', () => {
+          // Get the selected file
+          const selectedFile = fileInput.files[0];
+  
+          if (selectedFile) {
+            // Create a FileReader object
+            const reader = new FileReader();
+  
+            // Define an event handler for when the file is loaded
+            reader.onload = (event) => {
+              const fileContents = event.target.result;
+  
+              // Detect the separator (either comma or semicolon)
+              let separator = ',';
+              if (fileContents.includes(';')) {
+                separator = ';';
+              }
+  
+              // Split the file contents by the detected separator
+              const lines = fileContents.split('\n');
+              lines.forEach((line) => {
+                const values = line.split(separator);
+                console.log('CSV Line:', values);
+  
+                // Store each value in the csvData array
+                csvData.push(values);
+              });
+  
+              // Display CSV data as an HTML table
+              const table = document.createElement('table');
+              table.className = 'csv-table';
+              csvData.forEach((row) => {
+                const tr = document.createElement('tr');
+                row.forEach((cell) => {
+                  const td = document.createElement('td');
+                  td.textContent = cell;
+                  tr.appendChild(td);
+                });
+                table.appendChild(tr);
+              });
+  
+              // Clear previous table (if any) and append the new one
+              while (csvTable.firstChild) {
+                csvTable.removeChild(csvTable.firstChild);
+              }
+              csvTable.appendChild(table);
+            };
+  
+            // Read the file as text
+            reader.readAsText(selectedFile);
+          }
+        });
+      }
+  
+      fireChanged() {
+        console.log('OnClick Triggered');
+      }
     }
-
+  
     customElements.define('custom-button', MasterData_Maintain);
-})();
+  })();
+  

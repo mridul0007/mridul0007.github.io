425b86d6c5a06732933f95529ae9759b38addc00
mridul0007 <mridul0007@gmail.com>
2023-09-05 10:55:34 +0200

    new push column names

--- csv_widget/h.js
+++ csv_widget/h.js
@@ -128,134 +128,111 @@
       </div>
     `;
   
-    class MasterData_Maintain extends HTMLElement {
-      constructor() {
-        super();
-        this.init();
-      }
-  
-      init() {
-        let shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(tmpl.content.cloneNode(true));
-  
-        const fileInput = shadowRoot.getElementById('input_box');
-        const fileNameInput = shadowRoot.getElementById('file_name');
-        const selectFileButton = shadowRoot.getElementById('select_file_button');
-        const uploadButton = shadowRoot.getElementById('upload_button');
-        const csvTable = shadowRoot.getElementById('csv_table');
-  
-        let csvData = []; // Array to store CSV data
-  
-        // Add a click event listener to the "Select File" button
-        selectFileButton.addEventListener('click', () => {
-          fileInput.click(); // Trigger a click event on the hidden file input
-        });
-  
-        // Add a change event listener to the file input
-        fileInput.addEventListener('change', () => {
-          // Display the selected file name in the text input
-          fileNameInput.value = fileInput.files[0].name;
-        });
-  
-        // Add a click event listener to the "Upload" button
-        uploadButton.addEventListener('click', () => {
-          // Get the selected file
-          const selectedFile = fileInput.files[0];
-  
-          if (selectedFile) {
-            // Create a FileReader object
-            const reader = new FileReader();
-  
-            // Define an event handler for when the file is loaded
-            reader.onload = (event) => {
-              const fileContents = event.target.result;
-  
-              // Detect the separator (either comma or semicolon)
-              let separator = ',';
-              if (fileContents.includes(';')) {
-                separator = ';';
-              }
-  
-              // Split the file contents by the detected separator
-              const lines = fileContents.split('\n');
-              lines.forEach((line) => {
-                const values = line.split(separator);
-                console.log('CSV Line:', values);
-  
-                // Store each value in the csvData array
-                csvData.push(values);
-              });
-
-
-
-
-
-                let id, description;
-                let remainingValues = [];
-
-                // Assuming you have parsed the CSV data into the csvData array
-
-                // Loop through each row
-                csvData.forEach((row, rowIndex) => {
-                // Loop through each column in the row
-                row.forEach((cellValue, colIndex) => {
-                    if (rowIndex === 0 && colIndex === 0) {
-                    // First row, first column
-                    id = cellValue;
-                    } else if (rowIndex === 0 && colIndex === 1) {
-                    // First row, second column
-                    description = cellValue;
-                    } else {
-                    // All other values
-                    remainingValues.push(cellValue);
-                    }
-                });
-                console.log('ID:', id);
-            console.log('Description:', description);
-            console.log('Remaining Values:', remainingValues);
-            id = '';
-            description = '';
-            remainingValues =[];
-            });
-
-    
-            
-
-
-
-
-  
-              // Display CSV data as an HTML table
-              const table = document.createElement('table');
-              table.className = 'csv-table';
-              csvData.forEach((row) => {
-                const tr = document.createElement('tr');
-                row.forEach((cell) => {
-                  const td = document.createElement('td');
-                  td.textContent = cell;
-                  tr.appendChild(td);
-                });
-                table.appendChild(tr);
-              });
-  
-              // Clear previous table (if any) and append the new one
-              while (csvTable.firstChild) {
-                csvTable.removeChild(csvTable.firstChild);
-              }
-              csvTable.appendChild(table);
-            };
-  
-            // Read the file as text
-            reader.readAsText(selectedFile);
+    (function () {
+        let tmpl = document.createElement('template');
+        tmpl.innerHTML = `
+          <div class="root">
+            <label for="input_box">Upload file:</label>
+            <input type="file" id="input_box" accept=".csv" style="display: none;">
+            <input type="text" id="file_name" placeholder="Select a CSV file..." readonly>
+            <button id="select_file_button">Select File</button>
+            <button id="upload_button">Upload</button>
+            <table id="csv_table">
+              <!-- Table headers will be added here -->
+            </table>
+          </div>
+        `;
+      
+        class MasterData_Maintain extends HTMLElement {
+          constructor() {
+            super();
+            this.init();
           }
-        });
-      }
-  
-      fireChanged() {
-        console.log('OnClick Triggered');
-      }
-    }
-  
-    customElements.define('custom-button', MasterData_Maintain);
-  })();
-  
+      
+          init() {
+            let shadowRoot = this.attachShadow({ mode: 'open' });
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+      
+            const fileInput = shadowRoot.getElementById('input_box');
+            const fileNameInput = shadowRoot.getElementById('file_name');
+            const selectFileButton = shadowRoot.getElementById('select_file_button');
+            const uploadButton = shadowRoot.getElementById('upload_button');
+            const csvTable = shadowRoot.getElementById('csv_table');
+      
+            let csvData = []; // Array to store CSV data
+            let columnNames = []; // Array to store column names
+      
+            // Add a click event listener to the "Select File" button
+            selectFileButton.addEventListener('click', () => {
+              fileInput.click(); // Trigger a click event on the hidden file input
+            });
+      
+            // Add a change event listener to the file input
+            fileInput.addEventListener('change', () => {
+              // Display the selected file name in the text input
+              fileNameInput.value = fileInput.files[0].name;
+            });
+      
+            // Add a click event listener to the "Upload" button
+            uploadButton.addEventListener('click', () => {
+              // Get the selected file
+              const selectedFile = fileInput.files[0];
+      
+              if (selectedFile) {
+                // Create a FileReader object
+                const reader = new FileReader();
+      
+                // Define an event handler for when the file is loaded
+                reader.onload = (event) => {
+                  const fileContents = event.target.result;
+      
+                  // Detect the separator (either comma or semicolon)
+                  let separator = ',';
+                  if (fileContents.includes(';')) {
+                    separator = ';';
+                  }
+      
+                  // Split the file contents by the detected separator
+                  const lines = fileContents.split('\n');
+      
+                  // Extract the header row (first row)
+                  const headerRow = lines[0];
+                  columnNames = headerRow.split(separator).map((columnName) => columnName.trim());
+                  console.log(columnNames);
+      
+                  // Remove any existing table headers
+                  while (csvTable.firstChild) {
+                    csvTable.removeChild(csvTable.firstChild);
+                  }
+      
+                  // Create table headers from column names
+                  const headerRowElement = document.createElement('tr');
+                  columnNames.forEach((columnName) => {
+                    const headerCell = document.createElement('th');
+                    headerCell.textContent = columnName;
+                    headerRowElement.appendChild(headerCell);
+                  });
+                  csvTable.appendChild(headerRowElement);
+      
+                  // Continue processing the rest of the CSV data (excluding the header) if necessary
+                  for (let i = 1; i < lines.length; i++) {
+                    const values = lines[i].split(separator);
+                    console.log('CSV Line:', values);
+                    // Store or process the data as needed
+                  }
+                };
+      
+                // Read the file as text
+                reader.readAsText(selectedFile);
+              }
+            });
+          }
+      
+          fireChanged() {
+            console.log('OnClick Triggered');
+          }
+        }
+      
+        customElements.define('custom-button', MasterData_Maintain);
+      })();
+      

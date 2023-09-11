7ef6d5c31072e4da0d1760012da97512f33ccdee
mridul0007 <mridul0007@gmail.com>
2023-09-11 17:45:24 +0200

    new push

--- csv_widget/h.js
+++ csv_widget/h.js
@@ -125,13 +125,13 @@
         await setupDanfo();
         fileInput.click();
         const dataBinding = this.dataBindings.getDataBinding('exportDataSource');
-        var ds = this.exportDataSource.data;
+        var ds = await this.exportDataSource.data;
         console.log(ds);
 
-        var ds2 = this.dataBindings.getDataBinding().getDataSource();
+        var ds2 = await this.dataBindings.getDataBinding().getDataSource();
         console.log(ds2);
 
-        var ds3 = ds2.getResultSetData()
+        var ds3 = await ds2.getResultSetData()
         console.log(ds3);
         
          // Trigger a click event on the hidden file input

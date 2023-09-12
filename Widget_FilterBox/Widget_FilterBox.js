d94311c4570522e92835aa5c6b4276daa9096fcf
mridul0007 <mridul0007@gmail.com>
2023-09-12 11:24:31 +0200

    ids desc members

--- Widget_FilterBox/Widget_FilterBox.js
+++ Widget_FilterBox/Widget_FilterBox.js
@@ -2,6 +2,17 @@
     // Define the HTML template for your custom element
     let tmpl = document.createElement('template');
     tmpl.innerHTML = `
+    <style>
+      /* Add CSS for the "child" div */
+      .child {
+        display: none; /* Initially hide the "child" div */
+        flex-direction: column; /* Set flex-direction to column */
+        align-items: flex-start; /* Align items to the start */
+        background-color: #f0f0f0; /* Add a background color */
+        padding: 10px; /* Add some padding */
+        border: 2px solid #aaa; /* Add a border */
+      }
+    </style>
     <button id="filter_button">Filter</button>
     <div class="child">
     <label for="select_box_filter">Filter:</label>
@@ -29,9 +40,36 @@
         // Add a click event listener to the "filter_button"
         filterButton.addEventListener('click', async () => {
             // Call the function or perform actions when the button is clicked
-        const dataBinding = this.dataBindings.getDataBinding('exportDataSource');
-        var ds2 = await this.dataBindings.getDataBinding().getDataSource().getMembers('MDBELNR');
-        console.log(ds2);
+            if (childDiv.style.display === 'none') {
+                childDiv.style.display = 'flex';
+            } else {
+                childDiv.style.display = 'none';
+            }
+
+            const dataBinding = this.dataBindings.getDataBinding('exportDataSource');
+            var ds2 = await this.dataBindings.getDataBinding().getDataSource().getMembers('MDBELNR');
+            console.log(ds2);
+
+            var dimensions =  await this.dataBindings.getDataBinding().getDimensions();
+            // var members = ArrayUtils.create(Type.MemberInfo);
+            // var value = InputField_1.getValue();
+            var ids = [];
+            var desc = [];
+            var temp = '';
+            var members;
+
+            for (var i = 0; i < dimensions.length; i++) {
+                members =  await this.dataBindings.getDataBinding().getDataSource().getMembers(dimensions[i], {limit: 1000000});
+                for (var j = 0; j < members.length; j++) {
+                    temp = dimensions[i].id + ":" + members[j].id;
+                    ids.push(temp);
+                    temp = dimensions[i].description + ":" + members[j].description;
+                    desc.push(temp);
+                    
+                }
+            }
+            console.log("done");
+
 
 
         });

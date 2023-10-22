5f66c185a779d48f6b98a6f68b85eefcbb73d964
mridul0007 <mridul0007@gmail.com>
2023-10-22 17:04:43 +0200

    new push geo data

--- Echarts/chart.js
+++ Echarts/chart.js
@@ -47,23 +47,132 @@ const getScriptPromisify = (src) => {
           this._chart = echarts.init(this._shadowRoot.querySelector('#chart'));
         }
 
-        const geoMapOption = {
-          type: 'geo',
-          map: 'USA',
-          data: [
-            { name: 'California', value: 39500000 },
-            { name: 'Texas', value: 29000000 },
-            { name: 'Florida', value: 21500000 },
-            { name: 'New York', value: 20000000 },
-            { name: 'Illinois', value: 12500000 },
-          ],
-          itemStyle: {
-            color: '#fff',
-            borderColor: '#ccc',
+        const option = {
+          title : {
+              text : '2012 World GDP Top 8',
+              subtext : 'from baike （Billion $）',
+              x : 'center'
           },
-        };
+          tooltip : {
+              trigger: 'item',
+              showDelay: 0,
+              transitionDuration: 0.2,
+              formatter : function (params) {
+                  var value = params.value + '';
+                  value = value.replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
+                  return params.seriesName + '<br/>' + value + ' Billion';
+              }
+          },
+          toolbox: {
+              show : true,
+              x: 'right',
+              y: 'bottom',
+              feature : {
+                  mark : {show: true},
+                  dataView : {show: true, readOnly: false},
+                  restore : {show: true},
+                  saveAsImage : {show: true}
+              }
+          },
+          dataRange: {
+              orient: 'horizontal',
+              x : 'center',
+              y: 'center',
+              min: 2000,
+              max: 16000,
+              splitNumber: 0,            // 分割段数，默认为5
+              text:['16,000B','2,000B'],  
+              calculable : true,
+              itemWidth:40,
+              color: ['orangered','yellow','lightskyblue']
+          },
+          series : [
+              {
+                  name: 'Japan',
+                  type: 'map',
+                  mapType: 'world|Japan',
+                  mapLocation: {x:'5%', y:'10%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'Japan', value : 5963.9}
+                  ]
+              },
+              {
+                  name: 'China',
+                  type: 'map',
+                  mapType: 'world|Pakistan',
+                  mapLocation: {x:'30%', y:'10%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'China', value : 25}
+                  ]
+              },
+              {
+                  name: 'Japan',
+                  type: 'map',
+                  mapType: 'world|Japan',
+                  mapLocation: {x:'55%', y:'10%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'Japan', value : 5963.9}
+                  ]
+              },
+              {
+                  name: 'Germany',
+                  type: 'map',
+                  mapType: 'world|Germany',
+                  mapLocation: {x:'76%', y:'10%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'Germany', value : 3400.5}
+                  ]
+              },
+              {
+                  name: 'France',
+                  type: 'map',
+                  mapType: 'world|France',
+                  mapLocation: {x:'5%', y:'60%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'France', value : 2608.6}
+                  ]
+              },
+              {
+                  name: 'United Kingdom',
+                  type: 'map',
+                  mapType: 'world|United Kingdom',
+                  mapLocation: {x:'33%', y:'60%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'United Kingdom', value : 2440.5}
+                  ]
+              },
+              {
+                  name: 'Brazil',
+                  type: 'map',
+                  mapType: 'world|Brazil',
+                  mapLocation: {x:'55%', y:'60%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'Brazil', value : 2395.9}
+                  ]
+              },
+              {
+                  name: 'Russia',
+                  type: 'map',
+                  mapType: 'world|Russia',
+                  mapLocation: {x:'76%', y:'70%',width:'22%',height:'35%'},
+                  itemStyle: itemStyle,
+                  data:[
+                      {name : 'Russia', value : 2021.9}
+                  ]
+              }
+          ]
+      };
+                          
 
-        this._chart.setOption(geoMapOption);
+        this._chart.setOption(option);
       } catch (error) {
         console.error('An error occurred:', error);
       }

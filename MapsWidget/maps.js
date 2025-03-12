e69098dbc4e6f1dec768a11506c2da19a34392c4
mridul0007 <mridul0007@gmail.com>
2025-03-12 23:48:06 +0100

    new size

--- MapsWidget/maps.js
+++ MapsWidget/maps.js
@@ -40,8 +40,8 @@
                 display:none;
             }
             #d-os-map {
-                height: 100%;
-                width: 100%;
+                height: 500px;
+                width: 400px;
                 display: none;
                 overflow: hidden;
             }

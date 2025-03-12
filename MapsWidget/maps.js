9182f2a769b0085b8f3aaa902e1ede841c76eaa6
mridul0007 <mridul0007@gmail.com>
2025-03-12 16:14:29 +0100

    changes

--- MapsWidget/maps.js
+++ MapsWidget/maps.js
@@ -437,7 +437,7 @@
             
             const loadingOverlay = this.shadowRoot.querySelector('#d-loading-overlay');
             const osMapContainer = this.shadowRoot.getElementById('d-os-map');
-            
+            osMapContainer.style.display = 'flex';
 
             // Check if leaflet is available
             if (!window.L) {

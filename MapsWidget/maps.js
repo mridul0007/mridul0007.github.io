dffeae1aba489c0bb41761c915687aca1fc7f53b
mridul0007 <mridul0007@gmail.com>
2025-03-17 23:51:35 +0100

    new animation

--- MapsWidget/maps.js
+++ MapsWidget/maps.js
@@ -405,8 +405,8 @@ class CombinedMap extends HTMLElement {
         this.shadowRoot.querySelector('#d-loading-overlay').style.display = 'none';
         this.shadowRoot.querySelector('#d-google-map').style.display = 'block';
         }
-        this.shadowRoot.querySelector('#d-google-map').style.display = 'block';
-        this.shadowRoot.querySelector('#d-loading-overlay').style.display = 'none';
+        // this.shadowRoot.querySelector('#d-google-map').style.display = 'block';
+        // this.shadowRoot.querySelector('#d-loading-overlay').style.display = 'none';
                
     }
 

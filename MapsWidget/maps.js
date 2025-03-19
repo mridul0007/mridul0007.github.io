13bdf8a313b82756c8d259c2f36d15ae718a5bb5
mridul0007 <mridul0007@gmail.com>
2025-03-19 13:36:00 +0100

    new push new

--- MapsWidget/maps.js
+++ MapsWidget/maps.js
@@ -187,7 +187,7 @@ class CombinedMap extends HTMLElement {
 
         
         if (this.mapType === 'google' && this.DB_COORDINATE_DATA.length > 0) {
-            this.set_view_loadingScreen_overlay();
+            // this.set_view_loadingScreen_overlay();
             if(this.FE_GM_MARKERS.length === 0)
             {
                 await this.fe_render_gMap();
@@ -195,7 +195,7 @@ class CombinedMap extends HTMLElement {
             else
             {
                 this.set_view_gMap();
-                this.set_view_loadingScreen_overlay();
+                // this.set_view_loadingScreen_overlay();
             }
             
         } else if (this.mapType === 'osm' && this.DB_COORDINATE_DATA.length > 0) {
@@ -207,7 +207,7 @@ class CombinedMap extends HTMLElement {
                 else
                 {
                     this.set_view_osMap();
-                    this.set_view_loadingScreen_overlay();
+                    // this.set_view_loadingScreen_overlay();
                 }
             
         }

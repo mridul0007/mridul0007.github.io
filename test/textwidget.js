169f478d0cf23793749aa1bc3f48f3fcb25e50cc
mridul0007 <mridul0007@gmail.com>
2025-05-09 12:16:05 +0200

    Refactor text widget JSON by removing unused properties and updating author name

--- test/textwidget.js
+++ test/textwidget.js
@@ -1,24 +1,39 @@
 // textwidget.js
-
-(function() {
-    let text = "";
-
-    // Custom Widget Constructor
-    let myWidget = {
-        init: function(container) {
-            this.container = container;
-            this.render();
-        },
-        render: function() {
-            this.container.innerHTML = `<div>${text}</div>`;
-        },
-        setText: function(newText) {
-            text = newText;
-            this.render();
-        }
-    };
-
-    // Register the widget
-    window.customWidgets = window.customWidgets || {};
-    window.customWidgets.TextWidget = myWidget;
-})();
+class CustomTextWidget extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+  
+      this.textElement = document.createElement("div");
+      this.textElement.style.padding = "10px";
+      this.textElement.style.border = "1px solid #ccc";
+      this.textElement.style.borderRadius = "5px";
+      this.textElement.style.cursor = "pointer";
+      this.textElement.style.background = "#f9f9f9";
+  
+      this.textElement.addEventListener("click", () => {
+        this.dispatchEvent(new CustomEvent("onCustomWidgetEvent", {
+          detail: {
+            name: "EVENT_CLICK"
+          }
+        }));
+      });
+  
+      this.shadowRoot.appendChild(this.textElement);
+    }
+  
+    // Called when widget is created or properties change
+    onCustomWidgetAfterUpdate(changedProps) {
+      if (changedProps.p_text !== undefined) {
+        this.textElement.textContent = changedProps.p_text;
+      }
+    }
+  
+    // Method exposed to scripting API
+    set_text(p_text) {
+      this.textElement.textContent = p_text;
+    }
+  }
+  
+  customElements.define("custom-textwidget", CustomTextWidget);
+  

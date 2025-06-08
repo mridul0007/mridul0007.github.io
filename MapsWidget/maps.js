f4ed1a4e031d0f8c9a081d8199ba9ca9d80553e0
mridul0007 <mridul0007@gmail.com>
2025-06-08 20:00:57 +0200

    Update maps.js

--- MapsWidget/maps.js
+++ MapsWidget/maps.js
@@ -354,7 +354,7 @@ class CombinedMap extends HTMLElement {
                     options: {
                         iconSize: [30, 30],
                         iconAnchor: [15, 30],
-                        popupAnchor: [-70, -20]
+                        popupAnchor: [-10, -10]
                     }
                 });
     
